5db27a35ac117dcb845970ad02b90736eb379711
PetroskyLuke <petroskyluke@gmail.com>
2020-03-18 18:57:28 -0400

    admin css/ a la carte editor
    changed some of the css for the admin page and the edit services page. also added the a la carte editor to the admin ui

--- website/admin/admin.php
+++ website/admin/admin.php
@@ -1,32 +1,5 @@
 ﻿<?php
-//BEGIN USER VERIFICATION
-//BEGIN SESSION CODE
-//get current time
-$time = $_SERVER['REQUEST_TIME'];
-//set the amount of time a session should live
-$timeout_duration = 60;
-//set parameters for session cookie storage
-ini_set('session.gc_maxlifetime', $timeout_duration);
-ini_set('session.cookie_lifetime', $timeout_duration);
-session_start();
-if(isset($_SESSION['LAST_ACTIVITY']) && 
-	(($_SESSION['LAST_ACTIVITY'] + $timeout_duration) < $time))
-{
-	session_unset();
-    session_destroy();
-    session_start(); 
-}
-if (session_status() == PHP_SESSION_NONE) {
-    session_start(); 
-  }
-
-$_SESSION['LAST_ACTIVITY'] = $time;
-//END SESSION CODE
-if(!isset($_SESSION["username"]))  
-{  
-	header("location:../login.php?msg=You+have+been+logged+out+due+to+inactivity");  
-}
-//END USER VERIFICATION
+include('../inc/session.php');
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -34,30 +7,19 @@ if(!isset($_SESSION["username"]))
     <title>Yencik Photography</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="../css/admin.css">
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-    <style>
-        body {
-            font-family: "Lato", sans-serif
-        }
-
-        .mySlides img {
-            width: auto;
-            height: 800px;
-            max-height: 800px;
-        }
-    </style>
 </head>
 
 <body>
 
     <!-- Page content -->
     <!-- Sidebar -->
-    <div class="w3-sidebar w3-light-grey w3-bar-block" style="width:25%">
-        <h3 class="w3-bar-item">Menu</h3>
-        <a href="#" class="w3-bar-item w3-button">Services</a>
-        <a href="editservices.php" class="w3-bar-item w3-button selected">Services</a>
+    <div class="sidebar w3-light-grey w3-bar-block">
+        <h3><a href="admin.php" class="w3-bar-item w3-button selected">Menu</a></h3>
+        <a href="editservices.php" class="w3-bar-item w3-button">Services</a>
         <a href="editportfolio.php" class="w3-bar-item w3-button">Portfolio</a>
         <a href="#" class="w3-bar-item w3-button">Reporting</a>
         <a href="../inc/logout.php" class="w3-bar-item w3-button">Logout</a>
@@ -65,18 +27,15 @@ if(!isset($_SESSION["username"]))
     </div>
 
     <!-- Page Content -->
-    <div style="margin-left:25%">
+    <div class="content">
 
         <div class="w3-container w3-teal">
-            <h1>My Page</h1>
+            <h1>Admin Page</h1>
         </div>
 
-        <img src="img_car.jpg" alt="Car" style="width:100%">
-
         <div class="w3-container">
-            <h2>Sidebar Navigation Example</h2>
-            <p>The sidebar with is set with "style="width:25%".</p>
-            <p>The left margin of the page content is set to the same value.</p>
+            <h2>Use this admin panel to edit the website</h2>
+            <p>use the side panel to get started</p>
         </div>
 
     </div>
--- website/admin/editservices.php
+++ website/admin/editservices.php
@@ -2,20 +2,31 @@
 include('../inc/session.php');
 //set variables
 if(!isset($submit)){$submit = filter_input(INPUT_POST,'submit');}
+
+//addon variables
 if(!isset($addon_ID)){$addon_ID = filter_input(INPUT_POST,'addon_ID');}
 if(!isset($addon_name)){$addon_name = filter_input(INPUT_POST,'addon_name');}
 if(!isset($addon_price)){$addon_price = filter_input(INPUT_POST,'addon_price');}
 if(!isset($addon_description)){$addon_description = filter_input(INPUT_POST,'addon_description');}
+
+//a-la-carte variables
+if(!isset($alct_ID)){$alct_ID = filter_input(INPUT_POST,'alct_ID');}
+if(!isset($alct_name)){$alct_name = filter_input(INPUT_POST,'alct_name');}
+if(!isset($alct_price)){$alct_price = filter_input(INPUT_POST,'alct_price');}
+if(!isset($alct_description)){$alct_description = filter_input(INPUT_POST,'alct_description');}
+
 //end set variables
+
 include_once('../inc/db_connect.php');
+
 //Form field is used to select which service needs to be edited
 if(!isset($typeofservice)){$typeofservice = filter_input(INPUT_POST, 'typeofservice');}
 $form_field1 = '<form action="" method="post">';
 $form_field1 .= '<select name="typeofservice" onchange="javascript:this.form.submit()">';
 $options = array();
-$options['Projects'] = 'Projects';
-$options['Add-ons'] = 'Add-ons';
-$options['A_la_carte'] = 'A la carte';
+$options['projects'] = 'projects';
+$options['add-on'] = 'add-on';
+$options['a_la_carte'] = 'a_la_carte';
 if (! empty($options)) {
     foreach ($options as $option) {
         $selected = ($typeofservice == $option) ? 'selected="selected"' : '';
@@ -25,7 +36,7 @@ if (! empty($options)) {
 $form_field1 .= '</select></form>';
 //end form field
 // Get addons
-if($typeofservice==='Add-ons'){
+if($typeofservice==='add-on'){
     //edit an addon row
     if(filter_input(INPUT_POST,'submit') === 'delete'){
         echo $addon_ID;
@@ -76,30 +87,31 @@ if($typeofservice==='Add-ons'){
     $rows = $statement->fetchAll();
     $statement->closeCursor();
     
-    $form_field2 = '';
+    $show_rows = '';
     //start table and add headers
-    $form_field2 .= '<table><tr><th>ID</th><th>add-on name</th><th>add-on price</th><th>add-on description</th></tr>';
+    $show_rows .= '<table><tr><th>ID</th><th>add-on name</th><th>add-on price</th><th>add-on description</th></tr>';
     if(!empty($rows)){
         foreach($rows as $row){
-            $form_field2 .= '<tr><form action="" method="post">';
-            $form_field2 .= '<td>'.$row['addon_ID'].'</td>
+            $show_rows .= '<tr><form action="" method="post">';
+            $show_rows .= '<td>'.$row['addon_ID'].'</td>
                             <td>'.$row['addon_name'].'</td>
                             <td>'.$row['addon_price'].'</td>
                             <td>'.$row['addon_description'].'</td>
                             <td><input type="submit" name="submit" value="edit"></td>
                             <td><input type="submit" name="submit" value="delete" id="delete_btn"></td>
 
-                            <input type="hidden" name="typeofservice" value="Add-ons">
+                            <input type="hidden" name="typeofservice" value="add-on">
                             <input type="hidden" name="addon_ID" value="'.$row['addon_ID'].'">
                             <input type="hidden" name="addon_name" value="'.$row['addon_name'].'">
                             <input type="hidden" name="addon_price" value="'.$row['addon_price'].'">
                             <input type="hidden" name="addon_description" value="'.$row['addon_description'].'">';
-            $form_field2 .= '</form></tr>';
+            $show_rows .= '</form></tr>';
         }
     }
+
     //this adds the row to add a new row of data into the addon table
-    $form_field2 .='<tr><form action="" method="post">
-                        <td>ID<input type="hidden" name="typeofservice" value="Add-ons"></td>
+    $show_rows .='<tr><form action="" method="post">
+                        <td>ID<input type="hidden" name="typeofservice" value="add-on"></td>
                         <td><input type="text" name="addon_name"></td>
                         <td><input type="text" name="addon_price"></td>
                         <td><input type="text" name="addon_description"></td>
@@ -107,7 +119,7 @@ if($typeofservice==='Add-ons'){
                         </form>
                     </tr>';
 
-    $form_field2 .= '</table>';
+    $show_rows .= '</table>';
     if(filter_input(INPUT_POST,'submit') === 'edit'){
         $form_field3 = '';
         $addon_ID = filter_input(INPUT_POST, 'addon_ID');
@@ -122,7 +134,7 @@ if($typeofservice==='Add-ons'){
                             <td><input type="text" name="addon_description" value="'.$row['addon_description'].'"></td>
                             <td>
                             <input type="submit" name="submit" value="change">
-                            <input type="hidden" name="typeofservice" value="Add-ons">
+                            <input type="hidden" name="typeofservice" value="add-on">
                             <input type="hidden" name="addon_ID" value="'.$row['addon_ID'].'">
                             </td>';
                 $form_field3 .= '</form></tr>';
@@ -132,7 +144,100 @@ if($typeofservice==='Add-ons'){
         $form_field3 .= '</table>';
     }
 }
+if($typeofservice==="a_la_carte"){
+    //delete an a-la-carte row query
+    if(filter_input(INPUT_POST,'submit') === 'delete'){
+        $alter = $db1->prepare('DELETE FROM a_la_carte
+                                WHERE a_la_carte_ID = :alct_ID');
+        $alter->bindParam(':alct_ID', $alct_ID);
+        $alter->execute();
+    }
+    //edit an a-la-carte row query
+    if(filter_input(INPUT_POST,'submit') === 'change'){
+        $alter = $db1->prepare('UPDATE a_la_carte
+                                SET a_la_carte_name = :alct_name, a_la_carte_price = :alct_price, a_la_carte_desc = :alct_description
+                                WHERE a_la_carte_ID = :alct_ID');
+        $alter->bindParam(':alct_ID', $alct_ID);
+        $alter->bindParam(':alct_name', $alct_name);
+        $alter->bindParam(':alct_price', $alct_price);
+        $alter->bindParam(':alct_description', $alct_description);
+        $alter->execute();
+    }
+    //add an a-la-carte row query
+    if(filter_input(INPUT_POST, 'submit') === 'add'){
+        
+        $insert = $db1->prepare('INSERT INTO a_la_carte (a_la_carte_name, a_la_carte_price, a_la_carte_desc) VALUES
+        (:alct_name, :alct_price, :alct_description)');
+        $insert->bindParam(':alct_name', $alct_name);
+        $insert->bindParam(':alct_price', $alct_price);
+        $insert->bindParam(':alct_description', $alct_description);
+        $insert->execute();
+    }
+    //display all a-la-carte rows
+    $query = 'SELECT a_la_carte_ID, a_la_carte_name, a_la_carte_price, a_la_carte_desc
+                FROM a_la_carte';
+    $statement = $db1->prepare($query);
+    $statement->execute();
+    $rows = $statement->fetchAll();
+    $statement->closeCursor();
+    
+    $show_rows = '';
+    //start table and add headers
+    $show_rows .= '<table><tr><th>ID</th><th>a la carte name</th><th>a la carte price</th><th>a la carte description</th></tr>';
+    if(!empty($rows)){
+        foreach($rows as $row){
+            $show_rows .= '<tr><form action="" method="post">';
+            $show_rows .= '<td>'.$row['a_la_carte_ID'].'</td>
+                            <td>'.$row['a_la_carte_name'].'</td>
+                            <td>'.$row['a_la_carte_price'].'</td>
+                            <td>'.$row['a_la_carte_desc'].'</td>
+                            <td><input type="submit" name="submit" value="edit"></td>
+                            <td><input type="submit" name="submit" value="delete" id="delete_btn"></td>
 
+                            <input type="hidden" name="typeofservice" value="a_la_carte">
+                            <input type="hidden" name="alct_ID" value="'.$row['a_la_carte_ID'].'">
+                            <input type="hidden" name="alct_name" value="'.$row['a_la_carte_name'].'">
+                            <input type="hidden" name="alct_price" value="'.$row['a_la_carte_price'].'">
+                            <input type="hidden" name="alct_description" value="'.$row['a_la_carte_desc'].'">';
+            $show_rows .= '</form></tr>';
+        }
+    }
+    //this adds insert row to the showrows string
+    $show_rows .='<tr><form action="" method="post">
+                        <td>ID<input type="hidden" name="typeofservice" value="a_la_carte"></td>
+                        <td><input type="text" name="alct_name"></td>
+                        <td><input type="text" name="alct_price"></td>
+                        <td><input type="text" name="alct_description"></td>
+                        <td><input type="submit" name="submit" value="add"></td>
+                        </form>
+                    </tr>';
+    $show_rows .= '</table>';
+
+
+    if(filter_input(INPUT_POST,'submit') === 'edit'){
+        $form_field3 = '';
+        $addon_ID = filter_input(INPUT_POST, 'addon_ID');
+        //start table and add headers
+        $form_field3 .= '<table><tr><th>ID</th><th>add-on name</th><th>add-on price</th><th>add-on description</th></tr>';
+        foreach($rows as $row){
+            if($row['a_la_carte_ID'] === $alct_ID){
+                $form_field3 .= '<tr><form action="" method="post">';
+                $form_field3 .= '<td>'.$row['a_la_carte_ID'].'</td>
+                            <td><input type="text" name="alct_name" value="'.$row['a_la_carte_name'].'"></td>
+                            <td><input type="text" name="alct_price" value="'.$row['a_la_carte_price'].'"></td>
+                            <td><input type="text" name="alct_description" value="'.$row['a_la_carte_desc'].'"></td>
+                            <td>
+                            <input type="submit" name="submit" value="change">
+                            <input type="hidden" name="typeofservice" value="a_la_carte">
+                            <input type="hidden" name="alct_ID" value="'.$row['a_la_carte_ID'].'">
+                            </td>';
+                $form_field3 .= '</form></tr>';
+            }
+            
+        }
+        $form_field3 .= '</table>';
+    }
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -140,23 +245,19 @@ if($typeofservice==='Add-ons'){
     <title>Yencik Photography</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="../css/admin.css">
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-    <style>
-        body {
-            font-family: "Lato", sans-serif
-        }
-    </style>
 </head>
 
 <body>
 
     <!-- Page content -->
     <!-- Sidebar -->
-    <div class="w3-sidebar w3-light-grey w3-bar-block" style="width:10%">
-        <h3 class="w3-bar-item">Menu</h3>
-        <a href="#" class="w3-bar-item w3-button">Services</a>
+    <div class="sidebar w3-light-grey w3-bar-block">
+        <h3><a href="admin.php" class="w3-bar-item w3-button">Menu</a></h3>
+        <a href="#" class="w3-bar-item w3-button selected">Services</a>
         <a href="editportfolio.php" class="w3-bar-item w3-button">Portfolio</a>
         <a href="reporting.php" class="w3-bar-item w3-button">Reporting</a>
         <a href="../inc/logout.php" class="w3-bar-item w3-button">Logout</a>
@@ -164,14 +265,14 @@ if($typeofservice==='Add-ons'){
     </div>
 
     <!-- Page Content -->
-    <div style="margin-left:10%">
+    <div class="content">
         <div class="w3-container w3-teal">
             <h1>Edit Services</h1>
         </div>
         <h3>This page can be used to edit the available services</h3>
         <h4>Please choose which type of service you would like to edit</h4>
         <?php echo $form_field1;?>
-        <?php if($typeofservice==="Add-ons"){ echo $form_field2; }?>
+        <?php if($typeofservice==="add-on"||$typeofservice==="a_la_carte"){ echo $show_rows; }?>
         </br>
         <?php if($submit==='edit'){ echo $form_field3; }?>
         

c359b18502a38edb8515229568feb1386a1aa52a
PetroskyLuke <petroskyluke@gmail.com>
2020-04-08 12:31:17 -0400

    Update index.php

--- website/index.php
+++ website/index.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <script src="js/jquery-3.4.1.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script>$("#theForm").ajaxForm({url:'gallery.php',type:'post'})</script>
     <title>Yencik Photography</title>
     <meta charset="UTF-8">

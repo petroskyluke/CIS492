c89cddc0c6b9e5a4199804b5dfb1dba0fb8f0229
PetroskyLuke <petroskyluke@gmail.com>
2020-04-09 13:56:12 -0400

    img changes

--- website/gallery.php
+++ website/gallery.php
@@ -25,8 +25,8 @@ for($i=1; $i<=6; $i++){?>
             }
         }
         echo "<div class='w3-center w3-container w3-section w3-large w3-text-white w3-display-bottommiddle' style='width:100%' />";
-        echo "<div class='w3-left w3-hover-text-khaki' onclick='plusDivs(-1,$i)'>&#10094;</div>";
-        echo "<div class='w3-right w3-hover-text-khaki' onclick='plusDivs(1,$i)'>&#10095;</div>";
+        echo "<div class='w3-left-align w3-half w3-hover-text-khaki' onclick='plusDivs(-1,$i)'>&#10094;</div>";
+        echo "<div class='w3-right-align w3-half w3-hover-text-khaki' onclick='plusDivs(1,$i)'>&#10095;</div>";
         echo "</div>";
         echo "</div>";
     ?>

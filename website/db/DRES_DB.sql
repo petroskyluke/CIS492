7c7ece3f912a4342b47269bee2f8804cbb0b6f4b
Kristopher31 <60152192+Kristopher31@users.noreply.github.com>
2020-04-18 12:52:47 -0400

    Update DRES_DB.sql

--- website/db/DRES_DB.sql
+++ website/db/DRES_DB.sql
@@ -36,7 +36,7 @@ CREATE TABLE login (
 CREATE TABLE packages (
 	package_ID			INT			NOT NULL	AUTO_INCREMENT,
 	package_name		VARCHAR(40)	NOT NULL,
-	package_price		VARCHAR(40)	NOT NULL,
+	package_price		DOUBLE		NOT NULL,
 	PRIMARY KEY (package_ID)
 );
 
@@ -51,7 +51,7 @@ CREATE TABLE package_features (
 CREATE TABLE add_ons (
 	addon_ID			INT				NOT NULL	AUTO_INCREMENT,
 	addon_name			VARCHAR(255)	NOT NULL,
-	addon_price			VARCHAR(255)	NOT NULL,
+	addon_price			DOUBLE			NOT NULL,
 	addon_description	VARCHAR(255)	NOT NULL,
 	PRIMARY KEY (addon_ID)
 );
@@ -59,7 +59,7 @@ CREATE TABLE add_ons (
 CREATE TABLE a_la_carte ( 
 	a_la_carte_ID		INT				NOT NULL	AUTO_INCREMENT,
 	a_la_carte_name		VARCHAR(255)	NOT NULL,
-	a_la_carte_price	VARCHAR(255)	NOT NULL,
+	a_la_carte_price	DOUBLE			NOT NULL,
 	a_la_carte_desc		VARCHAR(1024),
 	PRIMARY KEY (a_la_carte_ID)
 );
@@ -68,6 +68,8 @@ CREATE TABLE a_la_carte (
 CREATE TABLE order_form (
 	order_form_ID		INT				NOT NULL		AUTO_INCREMENT,
 	package_chosen		INT				NOT NULL,
+	addon_boxes_selected	VARCHAR(255),
+	a_la_carte_boxes_selected	VARCHAR(255),
 	email				VARCHAR(50)		NOT NULL,
 	phone				VARCHAR(50)		NOT NULL,
 	requested_date		DATE			NOT NULL,
@@ -75,20 +77,22 @@ CREATE TABLE order_form (
 	address2			VARCHAR(50),
 	city				VARCHAR(50)		NOT NULL,
 	province_state		VARCHAR(50)		NOT NULL,
+	zip_code			VARCHAR(10)		NOT NULL,
 	PRIMARY KEY (order_form_ID)
 );
+/*
 CREATE TABLE addon_detail (
 	addon_detail_ID		INT			NOT NULL 		AUTO_INCREMENT,
 	addon_ID			INT			NOT NULL,
 	order_form_ID		INT			NOT NULL,
-)
+)*/
 INSERT INTO login (username, password_) VALUES
 ('Josh', '$2y$10$hACDPch1eJLxB5SQf3IsfOQiNSiLgm.J6YdQMZ8LYB45I6LozpOVO');
 
 INSERT INTO packages (package_name,package_price) VALUES
-('1st Package','$150'),
-('2nd Package','$200'),
-('3rd Package','$320');
+('1st Package','150.00'),
+('2nd Package','200.00'),
+('3rd Package','320.00');
 
 INSERT INTO package_features (package_feature_name,package_feature_desc, package_ID) VALUES
 ('Matterport 3D Showcase','Have a 3D tour created', 1),
@@ -116,15 +120,15 @@ INSERT INTO package_features (package_feature_name,package_feature_desc, package
 
 
 INSERT INTO add_ons (addon_name,addon_price,addon_description) VALUES
-('2D Floor Plans','$25','View the location as a floor plan'),
-('Measurements of Rooms','$10','Receive room measurements/dimensions');
+('2D Floor Plans','25.00','View the location as a floor plan'),
+('Measurements of Rooms','10.00','Receive room measurements/dimensions');
 
 INSERT INTO a_la_carte (a_la_carte_name,a_la_carte_price,a_la_carte_desc) VALUES
-('25 Professional Pictures','$70','Receive 25 pictures of the property from our expert photographer'),
-('Drone Video (Outside Only)','$115','Receive video of the property from our drone'),
-('Drone Video & Interior Video','$215','Receive video of both the outside and inside of the location'),
-('Interior Video','$100','Receive video of the interior of the location'),
-('Virtual Staging','$45/picture','Have the interior design edited using digital editing'),
+('25 Professional Pictures','70.00','Receive 25 pictures of the property from our expert photographer'),
+('Drone Video (Outside Only)','115.00','Receive video of the property from our drone'),
+('Drone Video & Interior Video','215.00','Receive video of both the outside and inside of the location'),
+('Interior Video','100.00','Receive video of the interior of the location'),
+('Virtual Staging','45.00','Have the interior design edited using digital editing'),
 ('Sky and/or Grass Replacement','$8/picture','Have the Sky and/or Grass in the pictures replaces with pristine blues and greens using digital editing'),
 ('Twilight Picture Transformation','$10/picture','Have the pictures digitally edited to look as if they were taken among a beautiful twilight'),
 ('Measurements of All Rooms','$10','Receive the internal dimensions of each room');

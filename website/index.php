1a48a86b564c77f4e0eb6460bf163106d6e9b67e
PetroskyLuke <petroskyluke@gmail.com>
2020-05-01 10:28:05 -0400

    Update index.php

--- website/index.php
+++ website/index.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Hydro Hawk Photography Photography</title>
+    <title>Hydro Hawk Photography</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="css/index.css">

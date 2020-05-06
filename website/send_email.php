1bcc6be3a36c5da7c3a0f6f5e76750d384b96843
bkaufman17 <bkaufman17@yahoo.com>
2020-05-05 21:52:23 -0400

    Update send_email.php

--- website/send_email.php
+++ website/send_email.php
@@ -18,7 +18,7 @@ require 'vendor/autoload.php';
 
 // Replace sender@example.com with your "From" address.
 // This address must be verified with Amazon SES.
-$sender = 'hydrohawkllc@gmail.com';
+$sender = 'petroskyluke@gmail.com';
 //$senderName = 'Luke Petrosky';
 
 // Replace recipient@example.com with a "To" address. If your account
